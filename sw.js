2e08630bef5b678d87ba924d46cee9a7c69b1522
moseskaliak69 <moseskaliika69@gmail.com>
2026-09-06 13:03:33 +0300

    VibeSec refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shyaka-cup-pwa-v9-bugonga-portraits-restored';
+const CACHE = 'shyaka-cup-pwa-v10-vibesec';
 const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', event => {
