8df913b7de480128a46ec2907fd2e0e6ed747c87
moseskaliak69 <moseskaliika69@gmail.com>
2026-09-06 10:29:44 +0300

    Refresh PWA cache after player photo fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shyaka-cup-pwa-v7-public-modern';
+const CACHE = 'shyaka-cup-pwa-v8-player-photo-fix';
 const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', event => {
