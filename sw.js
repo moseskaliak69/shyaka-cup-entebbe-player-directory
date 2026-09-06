987cd0a9a01a91a42e7697b8be2a907c15a28795
moseskaliak69 <moseskaliika69@gmail.com>
2026-09-06 10:12:18 +0300

    Update PWA cache for modern public design

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shyaka-cup-pwa-v3-highlights';
+const CACHE = 'shyaka-cup-pwa-v7-public-modern';
 const OFFLINE_URL = '/index.html';
 
 self.addEventListener('install', event => {
@@ -8,7 +8,8 @@ self.addEventListener('install', event => {
     '/icon-192.png',
     '/icon-512.png',
     '/apple-touch-icon.png',
-    '/official-sponsor-shyaka.jpg'
+    '/official-sponsor-shyaka.jpg',
+    '/shyaka-cup-logo.jpg'
   ])));
   self.skipWaiting();
 });
